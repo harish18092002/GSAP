0c4f0dcd6d4957611d80761aea81ea99b6bf3af9
Harish A <harish18092002@gmail.com>
2024-02-21 21:48:34 +0530

    Gsap animate 1

--- src/app/animat1/animat1.component.ts
+++ src/app/animat1/animat1.component.ts
@@ -10,5 +10,24 @@ gsap.registerPlugin(ScrollTrigger);
   styleUrls: ['./animat1.component.css']
 })
 export class Animat1Component {
+  ngOnInit() {
+    this.setupGsap();
+  }
 
+  setupGsap(): void {
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: '.container',
+        start: 'top center', // when the top of the trigger hits the top of the viewport
+        end: '+=400', // end after scrolling 500px beyond the start
+        scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+      },
+    });
+
+    tl.from('.box', {
+      backgroundColor: '#28a92b',
+      rotation: 360,
+      scale: 0,
+    });
+  }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,24 +10,5 @@ import { Component, OnInit } from '@angular/core';
 
 export class AppComponent {
   title = 'scroll_velocity';
-  ngOnInit() {
-    this.setupGsap();
-  }
 
-  setupGsap(): void {
-    let tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: '.container',
-        start: 'top center', // when the top of the trigger hits the top of the viewport
-        end: '+=400', // end after scrolling 500px beyond the start
-        scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
-      },
-    });
-
-    tl.from('.box', {
-      backgroundColor: '#28a92b',
-      rotation: 360,
-      scale: 0,
-    });
-  }
 }
