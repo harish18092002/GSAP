7dc4e166d35fe4a181235ad3e94b12e953ecb9ae
Harish A <harish18092002@gmail.com>
2024-02-26 23:11:47 +0530

    GSAP

--- src/app/animat1/animat1.component.ts
+++ src/app/animat1/animat1.component.ts
@@ -1,5 +1,6 @@
 import 'zone.js/dist/zone';
 import { Component, OnInit } from '@angular/core';
+// for gsap libraries
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 gsap.registerPlugin(ScrollTrigger);
