2f008a2b7723df3a7ed9fa822e49104842733cf2
Harish A <harish18092002@gmail.com>
2024-02-27 23:28:32 +0530

    Scroll velocity

--- src/app/animatesecond/animatesecond.component.ts
+++ src/app/animatesecond/animatesecond.component.ts
@@ -1,19 +1,35 @@
-import { Component, ElementRef, HostListener } from '@angular/core';
-import gsap from 'gsap'; // or import { gsap } from 'gsap/all';
 
+import { Component, OnInit } from '@angular/core';
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
 @Component({
   selector: 'app-animatesecond',
   templateUrl: './animatesecond.component.html',
   styleUrls: ['./animatesecond.component.css']
 })
-export class AnimatesecondComponent {
-  constructor(private elementRef: ElementRef) { }
 
-  @HostListener('window:scroll')
-  onScroll() {
-    const connectNowElement = this.elementRef.nativeElement.querySelector('.connect-now');
-    const scrollVelocity = window.scrollY;
 
-    gsap.to(connectNowElement, { x: scrollVelocity * 0.5 }); // Adjust the velocity multiplier as needed
+export class AnimatesecondComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    gsap.registerPlugin(ScrollTrigger);
+
+    gsap.to('.horizontal-section', {
+      xPercent: -100,
+      x: () => window.innerWidth,
+      ease: 'none',
+      scrollTrigger: {
+        trigger: '.horizontal-section',
+        start: 'top top',
+        end: () => window.innerWidth * 4,
+        scrub: 3,
+        pin: true,
+        invalidateOnRefresh: true,
+        anticipatePin: 1
+      }
+    });
   }
+
 }
