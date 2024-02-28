68a62f58ab6a4db6405be67181f0c12b05158316
Harish A <harish18092002@gmail.com>
2024-02-28 23:22:17 +0530

    Scroll velocity

--- src/app/animatesecond/animatesecond.component.ts
+++ src/app/animatesecond/animatesecond.component.ts
@@ -1,21 +1,49 @@
-
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import gsap from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
+
 @Component({
   selector: 'app-animatesecond',
   templateUrl: './animatesecond.component.html',
   styleUrls: ['./animatesecond.component.css']
 })
-
-
-export class AnimatesecondComponent implements OnInit {
+export class AnimatesecondComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
   ngOnInit(): void {
     gsap.registerPlugin(ScrollTrigger);
 
+    // Rotate the 'Vision' word in place
+    gsap.to('.vision', {
+      rotation: 360,
+      transformOrigin: 'center',
+      ease: 'none',
+      scrollTrigger: {
+        trigger: '.horizontal-section',
+        start: 'top top',
+        end: 'bottom bottom',
+        scrub: true
+      }
+    });
+
+    // Hide the 'Reality' word initially
+    gsap.set('.reality', { opacity: 0 });
+
+    // Show the 'Reality' word when it comes to the center of the screen
+    gsap.to('.reality', {
+      opacity: 1,
+      scrollTrigger: {
+        trigger: '.horizontal-section',
+        start: 'top center',
+        end: 'bottom center',
+        toggleActions: 'play none none none' // Play animation when the trigger is in the center of the viewport
+      }
+    });
+  }
+
+  ngAfterViewInit(): void {
+    // Horizontal scroll animation
     gsap.to('.horizontal-section', {
       xPercent: -100,
       x: () => window.innerWidth,
@@ -30,9 +58,8 @@ export class AnimatesecondComponent implements OnInit {
         anticipatePin: 1
       }
     });
-  }
-  ngAfterViewInit(): void {
+
+    // Animate the exclamation mark
     gsap.to('.exclamation', { y: -15, repeat: -1, yoyo: true, duration: 0.5 });
   }
-
 }
